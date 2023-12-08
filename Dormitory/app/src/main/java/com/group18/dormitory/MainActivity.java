382d4b4311e2f8c0aae1f5e65c416bd2f175c5c2
HaoTo203 <heohoi998@gmail.com>
2023-12-08 21:52:06 +0700

    SignIn/SignUp Home with bottom navigate

--- Dormitory/app/src/main/java/com/group18/dormitory/MainActivity.java
+++ Dormitory/app/src/main/java/com/group18/dormitory/MainActivity.java
@@ -1,8 +1,14 @@
 package com.group18.dormitory;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initiateData();
+
+    }
+
+    private void initiateData() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
